98d0c2f8f56557f5f97ff614eaead41630a57917
omezxyz <omezrva@gmail.com>
2025-09-26 10:51:43 +0530

    Updated edit form to update data and included extra field

--- src/utils/caseConverter.tsx
+++
@@ -1,9 +0,0 @@
-// utils/caseConverter.ts
-export const camelToSnake = (obj: Record<string, any>) => {
-  const newObj: Record<string, any> = {};
-  for (const key in obj) {
-    const snakeKey = key.replace(/([A-Z])/g, "_$1").toLowerCase();
-    newObj[snakeKey] = obj[key];
-  }
-  return newObj;
-};
